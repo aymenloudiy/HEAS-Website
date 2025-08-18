9911e2ee1b7129e96e3110e4502da03d0d0c2b9a
Nimuel <yiduolnemya@gmail.com>
2025-08-18 19:12:39 -0400

    chore: remove secondary hero for services

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,13 +1,13 @@
 import OurServices from "../components/OurServices";
 import ServicesHero from "../components/ServicesHero";
-import ServicesSecondaryHero from "../components/ServicesSecondaryHero";
+// import ServicesSecondaryHero from "../components/ServicesSecondaryHero";
 
 function Services() {
   return (
     <div>
       <ServicesHero />
       <OurServices />
-      <ServicesSecondaryHero />
+      {/* <ServicesSecondaryHero /> */}
     </div>
   );
 }
