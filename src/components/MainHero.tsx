226ffd6e9590452b164c6e872b40d92493dd44e3
Nimuel <yiduolnemya@gmail.com>
2025-08-18 19:10:40 -0400

    feat: add home video

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -1,7 +1,9 @@
 function MainHero() {
   return (
-    <div className="mx-32 border-2 h-64">
-      <video src=""></video>
+    <div className="h-fit">
+      <video autoPlay loop>
+        <source src="/src/assets/vecteezy_3d-render-animation-scene-living-room-interior-design_5018340.mp4" />
+      </video>
     </div>
   );
 }
