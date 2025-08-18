a42038b77fdf26fe0399e0b29d12d0ad497c6777
Nimuel <yiduolnemya@gmail.com>
2025-08-18 19:11:13 -0400

    fix: fix home screen video rendering compared to others

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  return (
+  const location = useLocation();
+  console.log(location);
+  return location.pathname == "/" ? (
+    <div>
+      <div className="relative">
+        <div className="h-fit absolute">
+          <video autoPlay>
+            <source src="/src/assets/vecteezy_3d-render-animation-scene-living-room-interior-design_5018340.mp4" />
+          </video>
+        </div>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <div className="relative grow">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  ) : (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="relative grow">
+      <div className="relative grow mt-32">
         <Outlet />
       </div>
       <Footer />
