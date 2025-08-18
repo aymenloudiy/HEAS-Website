9250636370731abee3194d2a3abb805b53e95682
Nimuel <yiduolnemya@gmail.com>
2025-08-18 19:12:57 -0400

    style: fix contact style

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,7 @@ import ContactUs from "../components/ContactUs";
 
 function Contact() {
   return (
-    <div className="absolute top-0 bottom-0 left-0 right-0 mx-32 flex items-stretch">
+    <div className="mx-32">
       <ContactUs />
     </div>
   );
