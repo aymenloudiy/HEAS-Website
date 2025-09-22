e9d22c365b4271c803879ed021c4b8bb8f2339e1
Nimuel <yiduolnemya@gmail.com>
2025-09-22 02:20:25 -0400

    fix black flash

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,10 @@ import { Outlet, useLocation } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { useState } from "react";
 
 function App() {
   const { pathname } = useLocation();
   const isHome = pathname === "/";
-  const videos = [
-    "/videos/Home_Video_1.mp4",
-    "/videos/Home_Video_2.mp4",
-    "/videos/Home_Video_3.mp4",
-    "/videos/Home_Video_4.mp4",
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handleEnded = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex + 1 < videos.length ? prevIndex + 1 : 0
-    );
-  };
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -31,14 +16,13 @@ function App() {
           <>
             <section className="relative min-h-screen">
               <video
-                key={currentIndex}
                 autoPlay
+                loop
                 muted
                 playsInline
-                onEnded={handleEnded}
                 className="absolute inset-0 h-full w-full object-cover"
               >
-                <source src={videos[currentIndex]} type="video/mp4" />
+                <source src="/videos/merged.mp4" />
               </video>
 
               <div className="relative z-10"></div>
